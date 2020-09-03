9eae3dfc3411bde0425da5b1c357f9adee5e1214
alex6470 <irimia_alex94@yahoo.com>
2020-09-03 17:44:51 +0300

    tema 1

--- src/components/bootcamp/bootcamp.js
+++ src/components/bootcamp/bootcamp.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import './bootcamp.css';
+<<<<<<< HEAD
 import {Link} from 'react-router-dom';
 
 const BootcampComponent = props => {
 
     const {bootcamp: {name, description, id}} = props;
     
+=======
+import moment from 'moment';
+
+const BootcampComponent = props => {
+    const {bootcamp: {name, description, date}, deleteThis, index} = props;
+>>>>>>> tema 1
     return (
         <div className="card-container">
+            <p>{index + 1 }</p>
             <p>{name}</p>
             <p>{description}</p>
+<<<<<<< HEAD
             <Link to={`/bootcamp/${id}`}>See details</Link>
+=======
+            <p>{moment(date).format('DD-MM-yyyy')}</p>
+            <button onClick = {deleteThis.bind(null,index)}>Delete</button>
+>>>>>>> tema 1
         </div>
     )
 }
